37899424973126cc18975e93edea7add22cffc13
Manjushaa@# <m.vetal1992@gmail.com>
2024-12-24 16:28:00 +0530

    LoanDisbursement var is added

--- src/main/java/com/cjc/app/Entity/LoanDisbursement.java
+++ src/main/java/com/cjc/app/Entity/LoanDisbursement.java
@@ -1,5 +1,30 @@
 package com.cjc.app.Entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
 public class LoanDisbursement {
+	
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer agreementId;
+	private Integer loanNo;
+	private String agreementDate;
+	private String amountPayType;
+	private Double totalAmount;
+	private String bankName;
+	private Long accountNumber;
+	private String IFSCCode;
+	private String accountType;
+	private Double transferAmount;
+	private String paymentStatus;
+	private String amountPaidDate;
+	
 
 }
