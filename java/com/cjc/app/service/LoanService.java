29d7569eda1220edd83892fc71642c168d5bed66
Manjushaa@# <m.vetal1992@gmail.com>
2024-12-25 14:45:38 +0530

    post,get,getall,delete endpoind is added

---
+++ java/com/cjc/app/service/LoanService.java
@@ -0,0 +1,22 @@
+package com.cjc.app.service;
+
+import java.util.List;
+
+import com.cjc.app.Entity.Customer;
+
+
+public interface LoanService {
+
+	Customer saveCustomer(Customer customer);
+
+	List<Customer> getAllCustomers();
+
+	Customer getCustomer(int customerId);
+
+	boolean deleteCustomer(int customerId);
+
+	
+
+	
+
+}
